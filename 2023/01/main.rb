f1fcf12d089923c89415ecff644a706f860e4cc7
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2023-12-03 14:32:41 +0100

    aoc2023: 1-3

---
+++ 2023/01/main.rb
@@ -0,0 +1,43 @@
+REMAP = {
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9",
+    "1" => "1",
+    "2" => "2",
+    "3" => "3",
+    "4" => "4",
+    "5" => "5",
+    "6" => "6",
+    "7" => "7",
+    "8" => "8",
+    "9" => "9"
+}
+
+def scan_1(line)
+    v = line.scan(/\d/)
+    [v.first, v.last].join.to_i
+end
+
+def scan_2(line)
+    k, idx = REMAP.keys
+                  .map { |k| [k, line.index(k)] }
+                  .select { |_, idx| idx && idx >= 0 }
+                  .min { |a, b| a.last <=> b.last }
+    k2, idx2 = REMAP.keys
+                    .map { |k| [k, line.rindex(k)] }
+                    .select { |_, idx| idx && idx >= 0 }
+                    .max { |a, b| a.last <=> b.last }
+    
+    [REMAP[k], REMAP[k2]].join.to_i
+end
+
+lines = ARGF.each_line.map { |l| l.chomp }
+
+puts lines.map { |l| scan_1(l) }.sum
+puts lines.map { |l| scan_2(l) }.sum
---
+++ 2023/03/main.rb
@@ -0,0 +1,61 @@
+SKIP_LIST = ('0'..'9').to_a + ['.']
+def adj?(schema, i0, j0)
+    (i0-1 .. i0+1).each do |i|
+        (j0-1 .. j0+1).each do |j|
+            next if i < 0 || i >= schema.length
+            next if j < 0 || j >= schema[i].length
+            
+            return [i, j] unless SKIP_LIST.include?(schema[i][j])
+        end
+    end
+
+    return nil
+end
+
+def collect(schema)
+    values = []
+    schema.each_with_index do |e, i|
+        value = ''
+        adj = false
+        adj_map = []
+
+        e.each_with_index do |v, j|
+            case v
+            when '0'..'9'
+                value += v
+                if a = adj?(schema, i, j)
+                    adj ||= true
+                    adj_map << {i: a.first, j: a.last, v: schema[a.first][a.last]}
+                end
+                
+            else
+                values << [value, adj_map.uniq] if value.size > 0 && adj
+                
+                value =  ''
+                adj_map = []
+                adj = false
+            end
+        end
+
+        values << [value, adj_map.uniq] if value.size > 0 && adj
+    end
+
+    return values
+end
+
+schema = ARGF.each_line
+             .map { |l| l.chomp }
+             .map { |l| l.chars }
+
+values = collect(schema)
+puts values.map { |x| x.first.to_i }.sum
+
+groups = values.inject({}) do |acc, v|
+    value, part = *v
+    acc.update(part.first => (acc[part.first] || []) << value.to_i)
+end
+
+puts groups.select { |k, _| k[:v] == '*' }
+        .select { |_, v| v.length == 2 }
+        .map { |_, v| v.inject(&:*) }
+        .sum
