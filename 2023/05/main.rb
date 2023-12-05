4fa7d34d05f8880dd659ac56e9d1368c0041b00c
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2023-12-05 22:45:31 +0100

    aoc2023: 05(2)

--- 2023/05/main.rb
+++ 2023/05/main.rb
@@ -1,8 +1,3 @@
-def readmap(lines)
-    lines.map { |x| x.split(' ').map(&:to_i) }
-         .map { |k, s, e| [s, k, e] }
-end
-
 def scanmap(name, lines)
     sidx = lines.index(name) + 1
     eidx = sidx
@@ -10,34 +5,40 @@ def scanmap(name, lines)
         eidx += 1
     end
 
-    readmap(lines[sidx..eidx-1])
+    lines[sidx..eidx-1].map { |x| x.split(' ').map(&:to_i) }
+                       .map { |k, s, e| [s, k, e] }
 end
 
 def find(map, val)
-    record = map.find { |s, k, e| s <= val && val <= s+e }
+    record = map.find { |s, k, e| s <= val && val < s+e }
     return val unless record 
 
     s, k, _ = *record
-
     return k + (val - s)
 end
 
-def find2(maps, id, val, lim)
-    return val if maps.size >= id
 
-    map = maps[id]
-    record = map.find { |s, k, e| s <= val && val <= s+e }
-    return [val, find2(maps, id, val+1, lim)].min
-
-    s, k, e = record
+def find2(map, val, count)
+    return [] if count <= 0
     
-    v1 = find2(maps, id+1, k + (val - s), 
-    if s + e < lim
-        v2 = find2(maps, id, s + e + 1, lim)
-        return [v1, v2].min
-    else
-        return v1
+    record = map.find { |s, k, e| s <= val && val < s+e  }
+    if record 
+        s, k, e = *record
+        
+        r = k + (val - s)
+        c = [e - (val - s), count].min
+        return [[r, c]] + find2(map, val+c, count-c)
     end
+        
+    record = map.find { |s, k, e| s <= val+count && val+count < s+e  }
+    if record 
+        s, k, e = *record
+        r = val
+        c = (s-1) - val
+        return [[r, c]] + find2(map, s, count-c)
+    end
+
+    return [[val, count]]
 end
 
 lines = ARGF.each_line.map(&:chomp)
@@ -54,12 +55,7 @@ maps = [
     'temperature-to-humidity map:',
     'humidity-to-location map:'].map{ |k| scanmap(k, lines) }
 
-
-
 puts seeds.map { |s| maps.inject(s) { |s, m| find(m, s) } }.min
-
-seeds_ranges = seeds.each_slice(2).to_a
-puts (seeds_ranges.inject(nil) do |a, e|
-    s, c = *e
-    find2(maps, s, c)
-end)
+puts maps.inject(seeds.each_slice(2).to_a) { |ranges, map| ranges.inject([]) { |acc, r| acc + find2(map, *r) } }
+         .map(&:first)
+         .min
