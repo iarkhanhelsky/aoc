be18bd18cf9e0bf8280ceb5efd408d802a18d54b
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2023-12-21 10:24:52 +0100

    aoc2023: 20(2)

--- 2023/20/solution.rb
+++ 2023/20/solution.rb
@@ -2,12 +2,16 @@ require "digest"
 require "json"
 
 class Network
+    attr_reader :journal 
+
     def initialize(configuration)
         @configuration = configuration.inject({}) { |acc, elem| acc.update(elem[1] => elem) }
         @state = configuration.inject({}) do |acc, elem|
             type, name, targets = *elem
             acc.update(name => {type: type, on: false, inputs: {}})
         end
+
+        @journal = @configuration.keys.inject({}) { |acc, elem| acc.update(elem => []) }
 
         @state.each do |k, v|
             if v[:type] == '&'
@@ -56,6 +60,7 @@ class Network
     def push
         queue = ['broadcaster']
         send('broadcaster', 'low', 'button')
+        @journal.each { |_, v| v << '|'}
         
 
         totals = {'low' => 1, 'high' => 0}
@@ -71,6 +76,7 @@ class Network
 
             type, _, targets = @configuration[name]
             pulse = state[:next]
+            @journal[name][-1] =  @journal[name][-1] + pulse[0]
 
             targets.each do |t|
                 # puts "#{name}  -#{pulse}-> #{t}"
@@ -92,13 +98,32 @@ class Solution
         low = 0
         high = 0
         net = Network.new(configuration)
-        1000.times do 
+        100000.times do 
             r = net.push
             low = low + r['low']
             high = high + r['high']
         end
 
+        net.journal.each do |k, v|        
+            hist = v.inject({}) { |a, e| a.update(e => ((a[e] || 0) + 1)) }
+            puts "#{k}: #{hist.inspect}"
+        end
+
+        ['mm', 'ff', 'fk', 'lh'].each do |n|
+            next unless net.journal[n]
+
+            print "#{n}: "
+            net.journal[n].each_with_index do |e, i|
+                if e.start_with?('|hl')
+                    print("#{i}, ")
+                end
+            end
+
+            puts 
+        end
+
         push = 0
+        # Network.new(configuration).estimate('rx')
         [low * high, push]
     end
 end
