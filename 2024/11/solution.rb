9477c17d8b01a4ae1f153facdc58efa21ba20f85
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2024-12-14 19:12:53 +0100

    2024: 11

--- 2024/11/solution.rb
+++ 2024/11/solution.rb
@@ -1,27 +1,33 @@
 class Solution
+  def initialize
+    @mem = {}
+  end
+
   def digits(s)
     Math.log10(s).floor + 1
   end
 
-  def blink(stones)
+  def blink(stone, round)
+    return 1 if round == 0
+    return @mem[[stone, round]] if @mem.key?([stone, round])
+
     result = []
-    stones.each do |s|
-      case
-      when s == 0
-        result << 1
-      when digits(s).even?
-        m = 10**(digits(s) / 2)
-        result += s.divmod(m)
-      else
-        result << s * 2024
-      end
+    case
+    when stone == 0
+      result << 1
+    when digits(stone).even?
+      m = 10**(digits(stone) / 2)
+      result += stone.divmod(m)
+    else
+      result << stone * 2024
     end
 
-    result
+    @mem[[stone, round]] = result.inject(0) {|a, s| a + blink(s, round - 1)}
   end
 
   def run(lines)
     stones = lines.first.split(' ').map(&:to_i)
-    yield 25.times.inject(stones) { |acc, _| blink(acc) }.size
+    yield stones.inject(0) {|acc, s| acc + blink(s, 25) }
+    yield stones.inject(0) {|acc, s| acc + blink(s, 75) }
   end
 end
